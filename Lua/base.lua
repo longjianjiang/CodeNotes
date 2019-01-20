3c933cc9aa77657bacc4e23a64a66929e88c0410
longjianjiang <616393956@qq.com>
2019-01-21 00:02:19 +0800

    Update(auto commit)

--- Lua/base.lua
+++ Lua/base.lua
@@ -6,3 +6,13 @@
 name = "nancy"
 print(name.."botwin")
 
+-- <Table>
+-- in lua, table can used like array(idx start from 1) or map
+arr = {1, 2, 3, 5, 7}
+for i,v in ipairs(arr) do
+	print("idx = "..i..", value = "..v)
+end
+dict = {"name": "nancy", "age": 17}
+for k,v in pairs(dict) do
+	print("key:"..k..",value:"..v)
+end
