f66538520798f8339d4b22cfdc561a176f5bcb76
longjianjiang <616393956@qq.com>
2019-01-21 00:27:54 +0800

    Update(auto commit)

--- Lua/base.lua
+++ Lua/base.lua
@@ -7,12 +7,15 @@ name = "nancy"
 print(name.."botwin")
 
 -- <Table>
--- in lua, table can used like array(idx start from 1) or map
+-- in lua, table can used like array(idx start from 1) or map,
+-- ipairs use (idx, value) form iterate array, pairs use (key, value) form iterate map.
 arr = {1, 2, 3, 5, 7}
 for i,v in ipairs(arr) do
 	print("idx = "..i..", value = "..v)
 end
-dict = {"name": "nancy", "age": 17}
+
+dict = {name="nancy"}
+dict.age = 17
 for k,v in pairs(dict) do
 	print("key:"..k..",value:"..v)
 end
